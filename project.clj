0c626018b03461249fcc9f41b82a525a6f3074a8
Andreas Steffan <a.steffan@contentreich.de>
2016-10-23 14:20:56 +0200

    - Add dep to our renderable version

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/deas/gorilla-plot"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [gorilla-renderable "2.0.0"]])
+                 [org.clojars.deas/gorilla-renderable "2.1.0-SNAPSHOT"]])
