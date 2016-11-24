9a15d2638486e02ebb9079093a1a8e44ffa60a66
Andreas Steffan <a.steffan@contentreich.de>
2016-11-24 14:45:04 +0100

    - Rolling to snapshot

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.deas/gorilla-plot "0.2.0"
+(defproject org.clojars.deas/gorilla-plot "0.2.1-SNAPSHOT"
   :description "A simple data-driven plotting library for Gorilla REPL."
   :url "https://github.com/deas/gorilla-plot"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojars.deas/gorilla-renderable "2.1.0"]])
+                 [org.clojars.deas/gorilla-renderable "2.2.0-SNASHOT"]])
