42118a67fd34b27295abf42c7ffb6036a2df9e95
CI/CD <ci@pinkgorilla.org>
2020-03-22 06:33:19 +0000

    Begin 0.9.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-plot "0.9.1"
+(defproject org.pinkgorilla/gorilla-plot "0.9.2-SNAPSHOT"
   :description "A simple data-driven plotting library for Gorilla REPL."
   :url "https://github.com/pink-gorilla/gorilla-plot"
   :license {:name "MIT"}
